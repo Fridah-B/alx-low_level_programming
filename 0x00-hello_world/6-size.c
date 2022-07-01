db591f843be949c5c5af598b058259b1f370cc2f
Daf <fridahbutale@gmail.com>
2022-07-01 08:46:13 -0700

    operating using sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Work done)
+ */
+int main(void)
+{
+	printf("Size of a char:%lu" sizeof(char) "byte(s)\n");
+	printf("Size of an int:%lu" sizeof(int) "byte(s)\n");
+	printf("Size of a long int:%lu" sizeof(long int) "byte(s)\n");
+	printf("Size of a long long int:%lu" sizeof(long long int) "byte(s)\n");
+	printf("Size of a float:%lu" sizeof(float) "byte(s)\n");
+	return (0);
+}
+
