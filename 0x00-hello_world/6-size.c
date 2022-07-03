1ce720aeac5741d0ccbcadcf96b35d852d945d77
Daf <fridahbutale@gmail.com>
2022-07-03 08:28:49 -0700

    getting size of various types on diff computers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,14 @@
 /**
  * main - Entry point
  *
- * Return: 0 (Work done)
+ * Return: 0 Always (Work done)
  */
 int main(void)
 {
-	printf("Size of a char:%lu" sizeof(char) "byte(s)\n");
-	printf("Size of an int:%lu" sizeof(int) "byte(s)\n");
-	printf("Size of a long int:%lu" sizeof(long int) "byte(s)\n");
-	printf("Size of a long long int:%lu" sizeof(long long int) "byte(s)\n");
-	printf("Size of a float:%lu" sizeof(float) "byte(s)\n");
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of an int: %lu byte(s)\n", sizeof(int));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
-
