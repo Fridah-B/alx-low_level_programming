85adf7aedcc563be9ecf824337962ad137bf77b5
Daf <fridahbutale@gmail.com>
2022-07-07 13:12:33 -0700

    uppercase check

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,14 @@
 #include "main.h"
 /**
  * _isupper - checks for lower case
- * @b - value to be compared
  * @c: checks for uppercase
  * Return: Always 0
  */
 int _isupper(int c)
 {
-	int b;
 
-	for (b = 'A'; b <= 'Z'; b++)
-	{
-	if (c == b)
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
-	}
-	return (0);
 }
