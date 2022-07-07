727bc8f15fb0c36ba530bc90fb57ba4406430e44
Daf <fridahbutale@gmail.com>
2022-07-07 16:29:47 -0700

    fizz buzz 2

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int y;
+
+	for (y = 1; y <= 100; y++)
+	{
+		if ((y % 15) == 0)
+			printf("FizzBuzz ");
+		else if ((y % 5) == 0)
+			printf("Buzz ");
+		else if ((y % 3) == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", y);
+	}
+	printf("\n");
+	return (0);
+}
