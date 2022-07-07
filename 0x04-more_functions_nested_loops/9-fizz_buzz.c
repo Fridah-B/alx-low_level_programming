6c9e49b25b77bb5d17833efbc009f17aae7bbaaa
Daf <fridahbutale@gmail.com>
2022-07-07 16:42:24 -0700

    fizz buzz 3

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,14 +11,16 @@ int main(void)
 
 	for (y = 1; y <= 100; y++)
 	{
-		if ((y % 15) == 0)
-			printf("FizzBuzz ");
-		else if ((y % 5) == 0)
-			printf("Buzz ");
-		else if ((y % 3) == 0)
-			printf("Fizz ");
+		if (y % 15 == 0)
+			printf("FizzBuzz");
+		else if (y % 5 == 0)
+			printf("Buzz");
+		else if (y % 3 == 0)
+			printf("Fizz");
 		else
-			printf("%d ", y);
+			printf("%d", y);
+		if (y != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
