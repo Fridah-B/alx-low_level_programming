d17baf7a9a09fb8790ece0e73cf65255027f5e7c
Daf <fridahbutale@gmail.com>
2022-07-21 12:22:32 -0700

    prints some numbers of a sring

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints even numbers
+ * @str - string to be printed
+ * Return: Always 0
+ */
+void puts2(char *str)
+{
+	int y;
+
+	for (y = 0; str[y] != '\0'; y++)
+	{
+	if (y % 2 == 0)
+		_putchar (str[y]);
+	}
+	_putchar ('\n');
+}
