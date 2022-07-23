09b5004342a2ffbbf63d676fd6111ec149ee13b1
Daf <fridahbutale@gmail.com>
2022-07-23 07:19:37 -0700

    exclude letters in alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-#include <conio.h>
 /**
  * main - Entry point
  *
@@ -10,9 +9,15 @@
 int main(void)
 {
 	int y;
-	removeChar (q, e)
-	for (y = 'A'; y <= 'Z'; y++)
-		putchar(tolower(y));
+
+	for (y = 'a'; y <= 'z'; y++)
+	{
+		if (y == 'q')
+			continue;
+		else if (y == 'e')
+			continue;
+		putchar (y);
+	}
 	putchar ('\n');
 	return (0);
 }
