c6b591e633cdd4368d5d61e70524087064cd3ae0
Daf <fridahbutale@gmail.com>
2022-07-24 04:12:02 -0700

    print hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int hex;
+
+	for (hex = '0'; hex <= '9'; hex++)
+		putchar(hex);
+	for (hex = 'a'; hex <= 'f'; hex++)
+		putchar(hex);
+	putchar ('\n');
+	return (0);
+}
